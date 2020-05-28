66a66e6f14b7e07a62eb6848308c7ad3b09795f7
Ricardo Ceregatti <ricardo.geofisica@gmail.com>
2020-05-28 16:41:49 -0300

    Simulador de dados

--- DataMining/rw_visual.py
+++ DataMining/rw_visual.py
@@ -11,7 +11,7 @@ while True :
     
     # Define o tamanho da janela de plotagem
     
-    plt.figure(figsize=(dpi=128,10,6))
+    plt.figure(figsize=(10,6))
     
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values,rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none',s=1)
