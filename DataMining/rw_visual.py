584526f4c8d82d11a04f7d311d7511264b552dc4
Ricardo Ceregatti <ricardo.geofisica@gmail.com>
2020-05-28 15:09:57 -0300

    Novos Gráficos

--- DataMining/rw_visual.py
+++ DataMining/rw_visual.py
@@ -6,10 +6,25 @@ while True :
     
  #Cria um passeio aleatorio e plota os pontos
 
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
-
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    
+    # Define o tamanho da janela de plotagem
+    
+    plt.figure(figsize=(dpi=128,10,6))
+    
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values,rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none',s=1)
+    
+    #plt.scatter(rw.x_values, rw.y_values, s=15)
+    
+    # Enfatiza o primeiro e o ultimo ponto
+    plt.scatter(0,0, c='green', edgecolors='none',s=45)
+    plt.scatter(rw.x_values[-1],rw.y_values[-1], c='red',edgecolors='none',s=45)
+    
+    #Remove os eixos
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
 
     plt.show() 
 
